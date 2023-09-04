fcde334693338f1b3065fd8ab340c9253345ebb2
shhossain <hossain0338@gmail.com>
2023-09-04 19:02:09 +0600

    bump

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = "0.0.2"
+version = "0.0.3"
 description = "A vector database for face embeddings or encodings"
 
 with open("README.md", encoding="utf-8") as f:
